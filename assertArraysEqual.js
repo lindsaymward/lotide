29346685264ac11cc4c30f94dd8b032845ea6160
Lindsay Ward <lward23@hotmail.com>
2023-05-02 15:37:30 -0700

    Use eqArrays and log match result

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arr1, arr2) {
+  let match;
+  for (let i = 0; i < arr1.length; i++) {
+      arr1[i] === arr2[i] ? match = true : match = false;
+    }
+  return match;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ ${array1} and ${array2} are equal!`);
+  } else {
+    console.log(`❌ ${array1} and ${array2} are not equal!`);
+  }
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, "3"], [1, 3, "2"]);
