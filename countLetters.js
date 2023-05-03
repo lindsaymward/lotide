cd9b8a78ffe48e27b9a26a56aad76e4e7ac5f260
Lindsay Ward <lward23@hotmail.com>
2023-05-03 13:07:12 -0700

    Create function with object return

--- countLetters.js
+++ countLetters.js
@@ -6,3 +6,32 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const countLetters = function(sentence) {
+  let count = {};
+  let lowerCaseSentence = sentence.toLowerCase();
+  for (let letter of lowerCaseSentence) {
+    if (letter !== " ") {
+      if (count[letter]) {
+        count[letter] += 1;
+      } else {
+        count[letter] = 1;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(countLetters("lighthouse in the house"));
+
+// Should = {
+//   l: 1,
+//   i: 2,
+//   g: 1,
+//   h: 4,
+//   t: 2,
+//   o: 2,
+//   u: 2,
+//   s: 2,
+//   e: 3,
+//   n: 1,
+// }
