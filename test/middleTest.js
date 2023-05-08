445f5324ac823483e06795540c7425fabe759e32
Lindsay Ward <lward23@hotmail.com>
2023-05-07 22:50:24 -0700

    Implement Mocha and Chai for middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+describe("#middle", () => {
+  it("returns second element in array for three element array [1,2,3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns two middle elements in new array when input array is even length [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("returns two middle elements with longer even length array [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
+});
