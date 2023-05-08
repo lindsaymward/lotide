f2ce3fdd246cd5a6d0733cce9da03718b7e8d490
Lindsay Ward <lward23@hotmail.com>
2023-05-07 22:13:13 -0700

    Put tests in separate file for middle

--- middle.js
+++ middle.js
@@ -1,19 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  let match;
-  for (let i = 0; i < arr1.length; i++) {
-    arr1[i] === arr2[i] ? match = true : match = false;
-  }
-  return match;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅ ${array1} and ${array2} are equal!`);
-  } else {
-    console.log(`❌ ${array1} and ${array2} are not equal!`);
-  }
-};
-
 const middle = function(array) {
   let middleIndex = Math.floor(array.length / 2);
   if (array.length === 1 || array.length === 2) {
@@ -27,7 +11,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
-console.log(middle([1, 2]));
+module.exports = middle;
