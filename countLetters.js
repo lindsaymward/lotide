7db64e56ab81cbbd28d7efa8059e720f445a48f4
Lindsay Ward <lward23@hotmail.com>
2023-05-08 11:07:43 -0700

    Remove testing from main countLetters file

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
 const countLetters = function(sentence) {
   let count = {};
   let lowerCaseSentence = sentence.toLowerCase();
@@ -21,17 +13,4 @@ const countLetters = function(sentence) {
   return count;
 };
 
-console.log(countLetters("lighthouse in the house"));
-
-// Should = {
-//   l: 1,
-//   i: 2,
-//   g: 1,
-//   h: 4,
-//   t: 2,
-//   o: 2,
-//   u: 2,
-//   s: 2,
-//   e: 3,
-//   n: 1,
-// }
+module.exports = countLetters;
